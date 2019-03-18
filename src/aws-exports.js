13e3289175f9c34bf865b6efb2b4999a7c94ef7f
RyHuey849 <33079962+RyHuey849@users.noreply.github.com>
2019-03-18 11:43:23 -0700

    First Commit

---
+++ src/aws-exports.js
@@ -0,0 +1,8 @@
+const awsConfig = {
+  identityPoolId: 'us-east-1_x86JWfH3k',
+  region: 'us-east-1',
+  userPoolId: 'us-east-1_x86JWfH3k',
+  userPoolWebClientId: '1kdp1d770sjgo3bsj6k0k2leb9'
+}
+
+export default awsConfig;
